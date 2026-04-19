9b3bf341c82dc3433560b914fca2669504b0d5ce
jenish445 <paudeljenish27@gmail.com>
2026-04-19 19:27:03 -0400

    Update createDDL.sql

--- createDDL.sql
+++ createDDL.sql
@@ -175,7 +175,7 @@ CREATE TABLE ACCOUNTING_TRANSACTION (
     payment_id INT,
 
     FOREIGN KEY (department_id) REFERENCES DEPARTMENT(department_id),
-    FOREIGN KEY (sale_id) REFERENCES SALE(sale_id),
+    FOREIGN KEY (sale_id) REFERENCES Sale(sale_id),
     FOREIGN KEY (service_id) REFERENCES SERVICE (service_id),
     FOREIGN KEY (payment_id) REFERENCES LOAN_PAYMENT(payment_id)
 );
