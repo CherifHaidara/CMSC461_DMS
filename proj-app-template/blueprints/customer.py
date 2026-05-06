83278a7e72693dd1b449696e74fa9ce59917d553
jenish445 <paudeljenish27@gmail.com>
2026-05-06 19:36:25 -0400

    Update customer.py

--- proj-app-template/blueprints/customer.py
+++ proj-app-template/blueprints/customer.py
@@ -1,1 +1,23 @@
+from flask import Blueprint, render_template
+From Utils import get_db_connection, login_required
 
+customer_bp = Blueprint('customer', __name__)
+
+@customer_bp.route('/customers')
+@login required
+
+def customer();
+
+  conn = get_db_connection()
+  cursor = conn.cursor()
+
+
+  query = "SELECT * FROM CUSTOMER" 
+  cursor.execute(query)
+
+  customers = cursor.fetchall()
+
+  cursor.close()
+  conn.close()
+
+  return render_template('customers.html', customers=customers)
