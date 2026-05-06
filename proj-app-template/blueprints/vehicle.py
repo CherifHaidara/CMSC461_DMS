a90cc281a9834d0478067e35aa4b785c74416c91
jenish445 <paudeljenish27@gmail.com>
2026-05-06 19:57:07 -0400

    Update vehicle.py

--- proj-app-template/blueprints/vehicle.py
+++ proj-app-template/blueprints/vehicle.py
@@ -1,27 +1,28 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash
 import mysql.connector
 from utils import get_db_connection, login_required
 
 vehicle_bp = Blueprint('vehicle', __name__)
 
 @vehicle_bp.route('/vehicles')
-@login required
+@login_required
+def vehicles():
 
-def vehicles();
+    vehicles_data = []
 
-  vehicles_data = []
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor(dictionary=True)
 
-  try:
-    conn = get_db_connection()
-    cursor = conn.cursor()
+        query = "SELECT * FROM VEHICLE"
+        cursor.execute(query)
 
-    query = "SELECT * FROM VEHICLE" 
-    cursor.execute(query)
+        vehicles_data = cursor.fetchall()
 
-  except mysql.connector.Error as err:
-    flash(f"MySQL Error: {err}", "error")
-    
-   finally:
+    except mysql.connector.Error as err:
+        flash(f"MySQL Error: {err}", "error")
+
+    finally:
         if 'cursor' in locals() and cursor is not None:
             cursor.close()
 
