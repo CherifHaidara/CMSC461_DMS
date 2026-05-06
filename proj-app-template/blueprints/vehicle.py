3fb9a47602613f758b3c3afcae407d5c3d82d445
jenish445 <paudeljenish27@gmail.com>
2026-05-06 19:39:26 -0400

    Update vehicle.py

--- proj-app-template/blueprints/vehicle.py
+++ proj-app-template/blueprints/vehicle.py
@@ -1,1 +1,23 @@
+from flask import Blueprint, render_template
+From Utils import get_db_connection, login_required
 
+customer_bp = Blueprint('vehicle', __name__)
+
+@customer_bp.route('/vehicles')
+@login required
+
+def vehicles();
+
+  conn = get_db_connection()
+  cursor = conn.cursor()
+
+
+  query = "SELECT * FROM VEHICLE" 
+  cursor.execute(query)
+
+  customers = cursor.fetchall()
+
+  cursor.close()
+  conn.close()
+
+  return render_template('customers.html', vehicles=vehicles)
