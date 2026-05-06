4883bbc2f198ac7c380ca37062e38a5e033a61c5
jenish445 <paudeljenish27@gmail.com>
2026-05-06 19:55:34 -0400

    Update customer.py

--- proj-app-template/blueprints/customer.py
+++ proj-app-template/blueprints/customer.py
@@ -5,32 +5,31 @@ from utils import get_db_connection, login_required
 customer_bp = Blueprint('customer', __name__)
 
 @customer_bp.route('/customers')
-@login required
+@login_required
 
-def customer();
+def customers():
 
   customers_data = []
+  try:
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    
+    query = "SELECT * FROM CUSTOMER"
+    cursor.execute(query)
+    
+    customers_data = cursor.fetchall()
+  
+  except mysql.connector.Error as err:
+    flash(f"MySQL Error: {err}", "error")
+  
+  finally:
+    if 'cursor' in locals() and cursor is not None:
+      cursor.close()
+    
+    if 'conn' in locals() and conn is not None and conn.is_connected():
+      conn.close()
 
-    try:
-        conn = get_db_connection()
-        cursor = conn.cursor(dictionary=True)
-
-        query = "SELECT * FROM CUSTOMER"
-        cursor.execute(query)
-
-        customers_data = cursor.fetchall()
-
-    except mysql.connector.Error as err:
-        flash(f"MySQL Error: {err}", "error")
-
-    finally:
-        if 'cursor' in locals() and cursor is not None:
-            cursor.close()
-
-        if 'conn' in locals() and conn is not None and conn.is_connected():
-            conn.close()
-
-    return render_template(
-        'customers.html',
-        customers=customers_data
-    )
+  return render_template(
+    'customers.html',
+    customers=customers_data
+  )
