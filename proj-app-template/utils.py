ce45ab3cda6bf6ea5be0a14723602f9ae09d0cbf
Cherif Haidara <haidaracherif024@gmail.com>
2026-05-07 11:32:47 -0400

    Update database password in utils.py

--- proj-app-template/utils.py
+++ proj-app-template/utils.py
@@ -8,7 +8,7 @@ def get_db_connection():
     return mysql.connector.connect(
         host="127.0.0.1",
         user="root",
-        password="543210Ta",
+        password="my-secret-pw",
         database="car_dealership",
         port=3306
     )
