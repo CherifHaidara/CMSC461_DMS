685b96adad435ddf048cf6bbd57938c0709a47fd
jenish445 <paudeljenish27@gmail.com>
2026-05-06 21:30:32 -0400

    Update customer.py

--- proj-app-template/blueprints/customer.py
+++ proj-app-template/blueprints/customer.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, flash
+from flask import Blueprint, render_template, flash, request, redirect, url_for
 import mysql.connector
 from utils import get_db_connection, login_required
 
@@ -33,3 +33,60 @@ def customers():
     'customers.html',
     customers=customers_data
   )
+@customer_bp.route('/customers/add', methods=['GET', 'POST'])
+@login_required
+
+
+def add_customer():
+
+    if request.method == 'POST':
+
+        customer_name = request.form['customer_name']
+        customer_address = request.form['customer_address']
+        customer_email = request.form['customer_email']
+        customer_phone = request.form['customer_phone']
+
+        try:
+            conn = get_db_connection()
+            cursor = conn.cursor()
+
+            query = """
+                INSERT INTO CUSTOMER
+                (
+                    customer_name,
+                    customer_address,
+                    customer_email,
+                    customer_phone
+                )
+                VALUES (%s, %s, %s, %s)
+            """
+
+            cursor.execute(
+                query,
+                (
+                    customer_name,
+                    customer_address,
+                    customer_email,
+                    customer_phone
+                )
+            )
+
+            conn.commit()
+
+            flash("Customer added successfully.", "success")
+
+            return redirect(url_for('customer.customers'))
+
+        except mysql.connector.Error as err:
+
+            flash(f"MySQL Error: {err}", "error")
+
+        finally:
+
+            if 'cursor' in locals() and cursor is not None:
+                cursor.close()
+
+            if 'conn' in locals() and conn is not None and conn.is_connected():
+                conn.close()
+
+    return render_template('add_customer.html')
